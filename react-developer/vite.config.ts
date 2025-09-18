df301341192c4463c12c80f0b1d7ea92093001b5
YazzerAbdalla <yasserabdalla149@gmail.com>
2025-09-18 17:42:04 +0300

    Init commit

---
+++ react-developer/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0", // Allow external connections
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true, // Enable polling for file changes in Docker
+      interval: 1000,
+    },
+  },
+});
